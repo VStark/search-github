cd85ddf520553af27fa52a4cce6ff29af090ae81
VStark <21239324+VStark@users.noreply.github.com>
2024-12-05 20:48:05 +0100

    VERSION: 1.1.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -70,8 +70,8 @@ android {
         applicationId = "com.sg"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
     packaging {
         resources {
